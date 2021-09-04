e68206358fd6d56d6ccb6bd89f6edae4e915ed90
ColMarek <colmarek254@gmail.com>
2021-09-04 21:18:40 +0300

    Update environment variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,6 @@ dotenv.config();
 export const config = {
   env: process.env.NODE_ENV || "development",
   botToken: process.env.DISCORD_BOT_TOKEN || "",
-  prefix: process.env.DISCORD_BOT_PREFIX || "!sozui",
   applicationId: process.env.APPLICATION_ID || "",
-  guildIds: process.env.GUILD_ID || "",
+  guildIds: process.env.GUILD_IDS || "",
 };
